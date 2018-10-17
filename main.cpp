34142c59219b0a6081504ce837e038c6564274c7
liao <284983325@qq.com>
2018-10-18 00:08:44 +0800

    Initially complete pid control

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <chrono>
 
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
@@ -13,7 +14,10 @@
 using namespace cv;
 using namespace GPIO;
 
+typedef std::chrono::time_point<std::chrono::system_clock> pid_time;
+
 const std::string CAM_PATH = "/dev/video0";
+const double PI = 3.1415926;
 const int CANNY_LOWER_BOUND = 100;
 const int CANNY_UPPER_BOUND = 200;
 const int MY_RHO = 2;
@@ -25,6 +29,18 @@ const double BOTTOM_DISTANCE_THRESHOLD = 50;
 const double BISECTOR_RATIO_THRESHOLD = 1.5590321636451379;
 const int SPEED = 50;
 const int TURN_ANGLE = 5;
+// Parameters for a PID controller
+const double K_P = 1.;
+const double K_I = 0.2;
+const double K_D = 0.2;
+const double SAMPLE_TIME = 30; // 30 milliseconds
+
+struct PIDParameters {
+    pid_time lastTime;
+    double lastError{0.};
+    double integralError{0.};
+
+};
 
 enum MoveState {
     move_forward,
@@ -38,12 +54,18 @@ double ratio(double x1, double y1, double x2, double y2);
 
 double dot(std::vector<double> vector1, std::vector<double> vector2);
 
-Vec4i findClosestLine(std::vector <Vec4i> *lines, double kernel_x, double kernel_y);
+Vec4i findClosestLine(std::vector<Vec4i> *lines, double kernel_x, double kernel_y);
 
 Vec4i findAngleBisector(const Vec4i &leftLine, const Vec4i &rightLine);
 
-MoveState generateNextMoveState(const Vec4i &leftLine, const Vec4i &rightLine, const Vec4i &bisector,
-                                const Vec4i &midLine);
+double pidControl(double error, PIDParameters &parameters);
+
+MoveState
+generateNextMoveState(const Vec4i &leftLine, const Vec4i &rightLine, const Vec4i &bisector, const Vec4i &midLine,
+                      PIDParameters &pidParameters);
+
+double nextMove(const Vec4i &leftLine, const Vec4i &rightLine, const Vec4i &bisector, const Vec4i &midLine,
+                PIDParameters &pidParameters);
 
 inline double distance(double x1, double y1, double x2, double y2) {
     return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
@@ -66,11 +88,15 @@ int main() {
 //
 //    std::clog << "Frame Size: " << width << height << std::endl;
 
-    Mat image = imread("/Users/jundaliao/CLionProjects/NaiveCar/examples/example5.jpg", IMREAD_COLOR);
+    Mat image = imread("../examples/example4.jpg", IMREAD_COLOR);
     Mat gray_image, contour;
 
     double height = image.rows;
     double width = image.cols;
+
+    // initialize a PIDParameters
+    PIDParameters pidParameters;
+    pidParameters.lastTime = std::chrono::system_clock::now();
 
     // Init the libGPIO
 #ifdef _ON_RASPBERRY
@@ -79,13 +105,14 @@ int main() {
 
     while (true) {
 //        capture >> image;
+        auto start = std::chrono::high_resolution_clock::now();
         if (image.empty())
             break;
 
         cvtColor(image, gray_image, COLOR_RGB2GRAY);
         Canny(gray_image, contour, CANNY_LOWER_BOUND, CANNY_UPPER_BOUND);
 
-        std::vector <Vec4i> lines, leftLines, rightLines;
+        std::vector<Vec4i> lines, leftLines, rightLines;
         HoughLinesP(contour, lines, MY_RHO, THETA, HOUGH_LINE_THRESHOLD, MIN_LINE_LENGTH, MAX_LINE_GAP);
 
         // take part into different types and filter useless lines
@@ -133,17 +160,27 @@ int main() {
         Vec4i midLine = Vec4i(int(kernel_x), 0, int(kernel_x), int(kernel_y));
         Vec4i bisector = findAngleBisector(leftLine, rightLine);
 
+        double turnAngleSin = nextMove(leftLine, rightLine, bisector, midLine, pidParameters);
+        double turnAngle = asin(turnAngleSin) * 180 / PI;
+        if (abs(turnAngle) > 45)
+            turnAngle = turnAngle < 0 ? -45 : 45;
+        std::cout << "sin of turn angle is : " << turnAngleSin << std::endl;
+        std::cout << "turn angle is " << turnAngle << std::endl;
+
+
+
 #ifdef _ON_RASPBERRY
-        MoveState nextAction = generateNextMoveState(leftLine, rightLine, bisector, midLine);
-
-        controlLeft(FORWARD, SPEED);
-        controlRight(FORWARD, SPEED);
-
-        if (nextAction == move_left) {
-            turnTo(-TURN_ANGLE);
-        } else if (nextAction == move_right) {
-            turnTo(TURN_ANGLE);
-        }
+//        MoveState nextAction = generateNextMoveState(leftLine, rightLine, bisector, midLine);
+//
+//        controlLeft(FORWARD, SPEED);
+//        controlRight(FORWARD, SPEED);
+//
+//        if (nextAction == move_left) {
+//            turnTo(-TURN_ANGLE);
+//        } else if (nextAction == move_right) {
+//            turnTo(TURN_ANGLE);
+//        }
+        turnTo(turnAngle);
 
         delay(1000);
 #endif
@@ -152,10 +189,11 @@ int main() {
 #ifdef _DEBUG
 
         std::cout << leftLine << " " << rightLine << " " << midLine << std::endl;
-        std::vector <Vec4i> test;
+        std::vector<Vec4i> test;
         test.push_back(leftLine);
         test.push_back(rightLine);
         test.push_back(midLine);
+        test.push_back(bisector);
 
         for (Vec4i vec: test) { // just show
             int x, y;
@@ -198,7 +236,7 @@ double dot(std::vector<double> vector1, std::vector<double> vector2) {
     return sum;
 }
 
-Vec4i findClosestLine(std::vector <Vec4i> *lines, double kernel_x, double kernel_y) {
+Vec4i findClosestLine(std::vector<Vec4i> *lines, double kernel_x, double kernel_y) {
     double dist = INFINITY;
     Vec4i vec4i;
     for (Vec4i vec: *lines) {
@@ -230,8 +268,9 @@ Vec4i findClosestLine(std::vector <Vec4i> *lines, double kernel_x, double kernel
 }
 
 
-MoveState generateNextMoveState(const Vec4i &leftLine, const Vec4i &rightLine, const Vec4i &bisector,
-                                const Vec4i &midLine) {
+MoveState
+generateNextMoveState(const Vec4i &leftLine, const Vec4i &rightLine, const Vec4i &bisector, const Vec4i &midLine,
+                      PIDParameters &pidParameters) {
     bool left = emptyLine(leftLine), right = emptyLine(rightLine);
     if (left && right)
         return move_forward;
@@ -241,6 +280,7 @@ MoveState generateNextMoveState(const Vec4i &leftLine, const Vec4i &rightLine, c
         return move_left;
 
     double leftRatio = ratio(leftLine), rightRatio = ratio(rightLine);
+
     if (leftRatio != INFINITY && rightRatio != INFINITY) {
         // Calculate distance between bottom intersection
         double midLineBottomX = midLine[0];
@@ -290,19 +330,82 @@ Vec4i findAngleBisector(const Vec4i &leftLine, const Vec4i &rightLine) {
 
     double l1, l2, v1x, v1y, v2x, v2y, v3x, v3y;
     v1x = leftLine[0] - leftLine[2];
-    v2x = rightLine[0] - rightLine[2];
+    v2x = -(rightLine[0] - rightLine[2]);
     v1y = leftLine[1] - leftLine[3];
-    v2y = rightLine[1] - rightLine[3];
+    v2y = -(rightLine[1] - rightLine[3]);
     l1 = sqrt(v1x * v1x + v1y * v1y);
     l2 = sqrt(v2x * v2x + v2y * v2y);
 
     v3x = v1x / l1 + v2x / l2;
     v3y = v1y / l1 + v2y / l2;
 
-    v3x+=x1;
-    v3y+=y1;
+    v3x += x1;
+    v3y += y1;
 
-    Vec4i bisector = Vec4i(x1,y1,v3x,v3y);
+    Vec4i bisector = Vec4i(x1, y1, v3x, v3y);
 
     return bisector;
+}
+
+double pidControl(double error, PIDParameters &parameters) {
+    auto currTime = std::chrono::system_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(currTime - parameters.lastTime).count();
+    std::cout << "duration : " << duration << std::endl;
+    duration *= 8;
+    if (duration > SAMPLE_TIME) {
+        double pTerm = error * K_P;
+
+        // I term
+        parameters.integralError += error * duration;
+        double iTerm = parameters.integralError * K_I / 100;
+        // D term
+        double delta_error = error - parameters.lastError;
+        double dTerm = (delta_error / duration) * K_D / 100;
+
+        // Update pid parameters
+        parameters.lastError = error;
+        parameters.lastTime = currTime;
+
+        std::cout << "pid parameters are :" << std::endl;
+        std::cout << "last error : " << parameters.lastError << std::endl;
+        std::cout << "integral error : " << parameters.integralError << std::endl;
+
+        std::cout << "p term is " << pTerm << "\ni term is "
+                  << iTerm << "\nd term is " << iTerm << std::endl;
+
+        return pTerm + iTerm + dTerm;
+    }
+
+    // just return 0(do nothing) if sample time is too short
+    return 0;
+}
+
+double nextMove(const Vec4i &leftLine, const Vec4i &rightLine, const Vec4i &bisector, const Vec4i &midLine,
+                PIDParameters &pidParameters) {
+    bool left = emptyLine(leftLine), right = emptyLine(rightLine);
+    if (left || right)
+        return 0;
+
+    double leftRatio = ratio(leftLine), rightRatio = ratio(rightLine);
+
+    if (leftRatio != INFINITY && rightRatio != INFINITY) {
+        // calculate current error which is also sin(angle between bisector and mid line)
+        double lenBisector = sqrt(bisector.ddot(bisector));
+        double lenMidLine = sqrt(midLine.ddot(midLine));
+
+        double cos = bisector.ddot(midLine) / (lenBisector * lenMidLine);
+        double sin = sqrt(1 - cos * cos);
+
+        // use bisector's slope to decide car's orientation
+        // if slope > 0 then the car heads left side, otherwise
+        // the car heads right side
+        double bisectorRatio = ratio(bisector);
+        if (bisectorRatio > 0) {
+            return pidControl(-sin, pidParameters);
+        } else {
+            return pidControl(sin, pidParameters);
+        }
+    }
+    return 0;
 }
